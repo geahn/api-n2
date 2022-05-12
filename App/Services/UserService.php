849f5af77e92459b7a0dc96f4df315425640f7b8
Geahn <contato@geahn.com>
2022-05-11 23:37:00 -0300

    make it better

---
+++ App/Services/UserService.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace App\Services;
+    use App\Models\User;
+
+    class UserService {
+        public function get($username = null) {
+            if ($username){
+                return User::select($username);
+            } else {
+                return User::selectAll();
+            }
+        }
+
+        public function post() {
+           $data = $_POST;
+           return  User::insert($data);
+        }
+    }
