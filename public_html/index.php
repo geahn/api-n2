8c5d475072715786ccb9ec151c5b1c1bb4cc59f2
Geahn <contato@geahn.com>
2022-05-22 17:57:12 -0300

    vai

--- public_html/index.php
+++ public_html/index.php
@@ -11,7 +11,9 @@
     header("Access-Control-Allow-Credentials", "true");
     header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     header("Access-Control-Max-Age", "3600");
-    header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+    header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Origin, Cache-Control, Pragma, Authorization, Accept-Encoding");
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+    header("Cache-Control: no-cache, must-revalidate"); 
 
     require_once '../vendor/autoload.php';
     
