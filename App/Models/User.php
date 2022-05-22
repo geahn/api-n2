da1425d2881a99080278159ebcdea23dd5dfd709
Geahn <contato@geahn.com>
2022-05-22 14:12:38 -0300

    vai

--- App/Models/User.php
+++ App/Models/User.php
@@ -15,7 +15,7 @@
             $numrows = pg_numrows($result);
 
             if (!$numrows) {
-            throw new \Exception("Nenhum usuário encontradox");
+            throw new \Exception("Nenhum usuário encontrado");
             exit;
             } else {
                 return $arr = pg_fetch_array($result, NULL, PGSQL_ASSOC);
