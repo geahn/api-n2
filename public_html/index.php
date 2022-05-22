2d2687aa7468f0fbe4c2c2738c77b72622957a76
Geahn <contato@geahn.com>
2022-05-22 17:58:18 -0300

    vai

--- public_html/index.php
+++ public_html/index.php
@@ -1,19 +1,13 @@
 <?php
     header('Content-Type: application/json');
 
-    // CORS HEADERS
-    // header("Access-Control-Allow-Origin: *");
-    // header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-    // header("Cache-Control: no-cache, must-revalidate"); 
+    // header("Access-Control-Allow-Origin", "*");
+    // header("Access-Control-Allow-Credentials", "true");
+    // header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    // header("Access-Control-Max-Age", "3600");
+    // header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Origin, Cache-Control, Pragma, Authorization, Accept-Encoding");
     // header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-
-    header("Access-Control-Allow-Origin", "*");
-    header("Access-Control-Allow-Credentials", "true");
-    header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    header("Access-Control-Max-Age", "3600");
-    header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Origin, Cache-Control, Pragma, Authorization, Accept-Encoding");
-    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-    header("Cache-Control: no-cache, must-revalidate"); 
+    // header("Cache-Control: no-cache, must-revalidate"); 
 
     require_once '../vendor/autoload.php';
     
