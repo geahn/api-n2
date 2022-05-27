4333423b8c3f63e91fabd918a92aeb091cf0e623
Geahn <contato@geahn.com>
2022-05-27 13:23:01 -0300

    ok

--- App/Models/User.php
+++ App/Models/User.php
@@ -53,7 +53,7 @@
             throw new \Exception("Falha ao inserir!");
             exit;
             } else {
-                return "Inserido com sucesso!";
+                return "Inserido com sucesso! Usuário: ".$username;
             }
         }
 
