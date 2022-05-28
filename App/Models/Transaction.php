21aebf66d4458a1ff3955ddf57006f13a40abdec
Geahn <contato@geahn.com>
2022-05-28 18:41:20 -0300

    vai

--- App/Models/Transaction.php
+++ App/Models/Transaction.php
@@ -9,11 +9,11 @@
             $con_string = 'host='.DBHOST.' port=5432 dbname='.DBNAME.' user='.DBUSER.' password='.DBPASS;
             $bdcon = pg_connect($con_string);
 
-            $result = pg_query($bdcon, "SELECT * FROM daniel_geahn.transactions INNER JOIN daniel_geahn.user_releases ON daniel_geahn.transactions.id = daniel_geahn.user_releases.user_id WHERE daniel_geahn.transactions.id =".$user_id);
+            $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id whereur.created_at between '2022-05-28' and '2022-05-28 23:59:59.999' and ur.user_id = ".$user_id);
             $numrows = pg_numrows($result);
 
             if (!$numrows) {
-            throw new \Exception("Nenhum usuário encontrado");
+            throw new \Exception("Nenhuma transação encontrada");
             exit;
             } else {
                 return $arr = pg_fetch_array($result, NULL, PGSQL_ASSOC);
