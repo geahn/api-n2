6a1ce0f3ed88230073721af267a1f42c6a9e5f87
Geahn <contato@geahn.com>
2022-05-28 16:31:18 -0300

    vai

--- App/Models/Transaction.php
+++ App/Models/Transaction.php
@@ -9,7 +9,7 @@
             $con_string = 'host='.DBHOST.' port=5432 dbname='.DBNAME.' user='.DBUSER.' password='.DBPASS;
             $bdcon = pg_connect($con_string);
 
-            $result = pg_query($bdcon, "SELECT * FROM daniel_geahn.transactions INNER JOIN daniel_geahn.user_releases ON daniel_geahn.transactions.id = daniel_geahn.user_releases.user_id");
+            $result = pg_query($bdcon, "SELECT * FROM daniel_geahn.transactions INNER JOIN daniel_geahn.user_releases ON daniel_geahn.transactions.id = daniel_geahn.user_releases.user_id WHERE daniel_geahn.transactions.id =".$user_id);
             $numrows = pg_numrows($result);
 
             if (!$numrows) {
