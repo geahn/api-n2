4ae7264b18272afda34161723bd300cdf2f8a19b
Geahn <contato@geahn.com>
2022-05-29 12:59:29 -0300

    vai

--- App/Models/Transaction.php
+++ App/Models/Transaction.php
@@ -11,7 +11,8 @@
             $con_string = 'host='.DBHOST.' port=5432 dbname='.DBNAME.' user='.DBUSER.' password='.DBPASS;
             $bdcon = pg_connect($con_string);
 
-            $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.created_at between '".$date."' and '".$date." 23:59:59.999' and ur.user_id = ".$user_id);
+            //$result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.created_at between '".$date."' and '".$date." 23:59:59.999' and ur.user_id = ".$user_id);
+            $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.user_id = ".$user_id);
             $numrows = pg_numrows($result);
 
             if (!$numrows) {
