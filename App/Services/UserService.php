a467ea5767dca548c5ba5ed98d16ef72383c115e
Geahn <contato@geahn.com>
2022-05-30 23:29:21 -0300

    vai

--- App/Services/UserService.php
+++ App/Services/UserService.php
@@ -17,15 +17,9 @@
             return  User::insert($data);
         }
 
-        public function put() {
-            // parse_str(file_get_contents('php://input'), $_PUT);
-            // $data = $_PUT;
-
-            $data = json_decode(file_get_contents('php://input'), true);
+        public function delete() {
+            parse_str(file_get_contents('php://input'), $_PUT);
+            $data = $_PUT;
             return  User::update($data);
-
-            //return $data = file_get_contents('php://input');
-            //$data = json_decode(file_get_contents('php://input'), true);
-            //$data = parse_str(file_get_contents('php://input'), true);
         }
     }
