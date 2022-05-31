82b8efbea56cd3533a38b9364ec8a2298e729f6c
Geahn <contato@geahn.com>
2022-05-31 00:57:40 -0300

    vai

--- App/Models/Transaction.php
+++ App/Models/Transaction.php
@@ -11,8 +11,8 @@
             $con_string = 'host='.DBHOST.' port=5432 dbname='.DBNAME.' user='.DBUSER.' password='.DBPASS;
             $bdcon = pg_connect($con_string);
 
-            //$result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.created_at between '".$date."' and '".$date." 23:59:59.999' and ur.user_id = ".$user_id);
-            $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.user_id = ".$user_id);
+            $result = pg_query($bdcon, "select t.*, ur.value value_u, ur.created_at updated_u from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on ur.transaction_id = t.id and ur.id = (select max(ur2.id) from daniel_geahn.user_releases ur2 where ur2.user_id = '".$user_id."' and ur2.transaction_id = t.id)");
+            // $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.user_id = ".$user_id);
             $numrows = pg_numrows($result);
 
             if (!$numrows) {
