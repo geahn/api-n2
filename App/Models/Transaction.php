99829ff5c119bf95ce71f47c9700286d48c767e6
Geahn <contato@geahn.com>
2022-05-31 00:59:17 -0300

    vai

--- App/Models/Transaction.php
+++ App/Models/Transaction.php
@@ -11,16 +11,16 @@
             $con_string = 'host='.DBHOST.' port=5432 dbname='.DBNAME.' user='.DBUSER.' password='.DBPASS;
             $bdcon = pg_connect($con_string);
 
-            $result = pg_query($bdcon, "select t.*, ur.value value_u, ur.created_at updated_u from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on ur.transaction_id = t.id and ur.id = (select max(ur2.id) from daniel_geahn.user_releases ur2 where ur2.user_id = '".$user_id."' and ur2.transaction_id = t.id)");
+            return $result = pg_query($bdcon, "select t.*, ur.value value_u, ur.created_at updated_u from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on ur.transaction_id = t.id and ur.id = (select max(ur2.id) from daniel_geahn.user_releases ur2 where ur2.user_id = '".$user_id."' and ur2.transaction_id = t.id)");
             // $result = pg_query($bdcon, "select t.* from daniel_geahn.transactions t inner join daniel_geahn.user_releases ur on t.id = ur.transaction_id where ur.user_id = ".$user_id);
-            $numrows = pg_numrows($result);
+            // $numrows = pg_numrows($result);
 
-            if (!$numrows) {
-            throw new \Exception("Nenhuma transação encontrada");
-            exit;
-            } else {
-                return $arr = pg_fetch_all($result, PGSQL_ASSOC);
-            }
+            // if (!$numrows) {
+            // throw new \Exception("Nenhuma transação encontrada");
+            // exit;
+            // } else {
+            //     return $arr = pg_fetch_all($result, PGSQL_ASSOC);
+            // }
         }
 
         public static function insert($data) {
